6faeb21c99b6a6d2d26f7b17c3639b99d179a3ac
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-07-28 00:59:14 +0700

    update calculation

--- main.js
+++ main.js
@@ -20,16 +20,45 @@ start.addEventListener("click", function () {
   num2.value = getRndInteger(1, 10);
 });
 var check = document.getElementById("check");
+var opt = document.getElementById("opt");
+var key = document.getElementById("key");
+var showKey = document.getElementById("showKey");
+function calcultion(num1, num2) {
+  var ketQua;
+  switch (opt.value) {
+    case "+":
+      ketQua = Number(num1.value) + Number(num2.value);
+      break;
+    case "-":
+      ketQua = Number(num1.value) - Number(num2.value);
+      break;
+    case "*":
+      ketQua = Number(num1.value) * Number(num2.value);
+      break;
+    case "/":
+      ketQua = Number(num1.value) / Number(num2.value);
+      break;
+    default:
+      break;
+  }
+  return ketQua
+};
+
 check.addEventListener("click", function () {
-  if (result.value == Number(num1.value) + Number(num2.value)) {
+  if (result.value == calcultion(num1,num2)) {
     alert("Đúng");
-     num1.value = getRndInteger(1, 10);
-     num2.value = getRndInteger(1, 10);
-     result.value  ="";
-     check.style.background = 'green';
+    num1.value = getRndInteger(1, 10);
+    num2.value = getRndInteger(1, 10);
+    result.value = "";
+    check.style.background = "green";
+    start.click();
   } else {
     alert("Sai");
-     check.style.background = "red";
-
+    check.style.background = "red";
   }
 });
+
+showKey.addEventListener('click', function(){
+    key.classList.toggle('show');
+    key.innerHTML = num1.value+' '+ opt.value+" "+ num2.value +" = "+ calcultion(num1, num2);
+})
