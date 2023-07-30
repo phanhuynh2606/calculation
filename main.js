6c40075c8edec189956ccf60a0429177f6d6a066
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-07-30 23:14:26 +0700

    update calculation divide

--- main.js
+++ main.js
@@ -18,14 +18,24 @@ var result = document.getElementById("result");
 start.addEventListener("click", function () {
   num1.value = getRndInteger(1, 10);
   num2.value = getRndInteger(1, 10);
- if(document.querySelector('.show')!= null){
-  showKey.click();
- }
+  if (document.querySelector(".show") != null) {
+    showKey.click();
+  }
 });
 var check = document.getElementById("check");
 var opt = document.getElementById("opt");
 var key = document.getElementById("key");
 var showKey = document.getElementById("showKey");
+function gcd(a, b) {
+  if (b === 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+}
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
 function calcultion(num1, num2) {
   var ketQua;
   switch (opt.value) {
@@ -39,16 +49,23 @@ function calcultion(num1, num2) {
       ketQua = Number(num1.value) * Number(num2.value);
       break;
     case "/":
-      ketQua = Number(num1.value) / Number(num2.value);
+      if (Number(num1.value) % Number(num2.value) == 0) {
+        ketQua = Number(num1.value) / Number(num2.value);
+      } else {
+        var ucln = gcd(Number(num1.value), Number(num2.value));
+        var res = Number(num1.value) / ucln + "/" + Number(num2.value) / ucln;
+        ketQua = res;
+      }
       break;
     default:
       break;
   }
-  return ketQua
-};
+
+  return ketQua;
+}
 
 check.addEventListener("click", function () {
-  if (result.value == calcultion(num1,num2)) {
+  if (result.value == calcultion(num1, num2)) {
     alert("Đúng");
     num1.value = getRndInteger(1, 10);
     num2.value = getRndInteger(1, 10);
@@ -61,7 +78,14 @@ check.addEventListener("click", function () {
   }
 });
 
-showKey.addEventListener('click', function(){
-    key.classList.toggle('show');
-    key.innerHTML = num1.value+' '+ opt.value+" "+ num2.value +" = "+ calcultion(num1, num2);
-})
+showKey.addEventListener("click", function () {
+  key.classList.toggle("show");
+  key.innerHTML =
+    num1.value +
+    " " +
+    opt.value +
+    " " +
+    num2.value +
+    " = " +
+    calcultion(num1, num2);
+});
